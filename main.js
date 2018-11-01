492e3ed32bce6cb9c42cf16685051ee0c0d4ea78
Thomas Wilburn <news@thomaswilburn.net>
2018-11-01 16:02:59 -0500

    First

---
+++ main.js
@@ -0,0 +1,158 @@
+var $ = (s, d = document) => Array.from(d.querySelectorAll(s));
+$.one = (s, d = document) => d.querySelector(s);
+
+var clockTime = $.one(".clock .time");
+var clockDate = $.one(".clock .date");
+
+var tick = function() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  if (hours > 12) hours -= 12;
+  seconds = (seconds + "").padStart(2, "0");
+
+  clockTime.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var day = days[now.getDay()];
+  var month = months[now.getMonth()];
+
+  clockDate.innerHTML = `${day}, ${month} ${now.getDate()}`;
+}
+
+setInterval(tick, 1000);
+tick();
+
+// Shadertoy code
+
+var canvas = document.querySelector(".background");
+var gl = canvas.getContext("webgl");
+var editorPanel = $.one(".editor");
+var textarea = $.one(".editor textarea");
+var editor;
+var errorDisplay = $.one(".editor .error");
+var showEditorButton = $.one(".show-editor");
+
+var vertex = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertex, `
+attribute vec2 coord;
+void main() {
+  gl_Position = vec4(coord, 0.0, 1.0);
+}
+`);
+gl.compileShader(vertex);
+  
+var compile = function() {
+  var fragment = gl.createShader(gl.FRAGMENT_SHADER);
+  var fragSource = editor ? editor.getValue() : textarea.value;
+  gl.shaderSource(fragment, fragSource);
+  gl.compileShader(fragment);
+
+  var error = gl.getShaderInfoLog(fragment);
+  if (error) {
+    errorDisplay.classList.add("show");
+    errorDisplay.innerHTML = error;
+    return;
+  }
+  errorDisplay.classList.remove("show");
+  localStorage.fragment = fragSource;
+
+  var program = gl.createProgram();
+  gl.attachShader(program, fragment);
+  gl.attachShader(program, vertex);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+  
+  // attributes and uniforms
+  gl.program = program;
+  gl.attributes = {
+    coord: 0
+  };
+  for (var a in gl.attributes) gl.attributes[a] = gl.getAttribLocation(program, a);
+  gl.uniforms = {
+    u_time: 0,
+    u_resolution: 0
+  };
+  for (var u in gl.uniforms) gl.uniforms[u] = gl.getUniformLocation(program, u);
+};
+
+var polys = [
+  -1, 1,
+  1, 1,
+  1, -1,
+  -1, 1,
+  1, -1,
+  -1, -1
+];
+var buffer = gl.createBuffer();
+
+var render = function(t) {
+  gl.enableVertexAttribArray(gl.uniforms.coords);
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(polys), gl.STATIC_DRAW);
+  gl.vertexAttribPointer(gl.uniforms.coords, 2, gl.FLOAT, false, 0, 0);
+  gl.uniform1f(gl.uniforms.u_time, t + 12581372.5324);
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.uniform2f(gl.uniforms.u_resolution, canvas.width, canvas.height);
+  gl.clearColor(0, 1, 1, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, polys.length / 2);
+  requestAnimationFrame(render);
+}
+
+var debounce = function(fn, delay = 1000) {
+  var timeout = null;
+  return function() {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(fn, delay);
+  }
+};
+
+var debouncedCompile = debounce(compile);
+
+var init = async function() {
+  var fragDefault = await (await fetch("defaultFragment.glsl")).text();
+  var fragStored = localStorage.fragment;
+  textarea.value = fragStored || fragDefault;
+  compile();
+  render();
+
+  $.one(".controls .reset").addEventListener("click", function() {
+    console.log("RESETTING");
+    if (editor) {
+      editor.setValue(fragDefault);
+    } else {
+      textarea.value = fragDefault;
+    }
+    compile();
+  })
+};
+
+var loadScript = function(url) {
+  return new Promise(ok => {
+    var script = document.createElement("script");
+    script.src = url;
+    script.onload = ok;
+    document.head.appendChild(script);
+  });
+};
+
+init();
+
+showEditorButton.addEventListener("click", async function() {
+  if (!editor) {
+    await loadScript("codemirror.js");
+    await loadScript("clike.js");
+    editor = CodeMirror.fromTextArea(textarea, {
+      theme: "material",
+      mode: "clike"
+    });
+    editor.on("change", debouncedCompile);
+  }
+  editorPanel.classList.toggle("show");
+});
